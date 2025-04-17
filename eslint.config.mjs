0a86a0cbe80d404e37460fb34339ef7246fabc40
Erol Zavidic <erolms@gmail.com>
2025-04-17 15:22:48 +0200

    chore: add linting with eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  {
+    // Exclude the dist folder from linting
+    ignores: ['dist/**/*'],
+  },
+);
