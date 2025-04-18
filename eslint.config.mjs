5a954a443ee0e0ec49df4f9aff14d7edcd9b9d45
Erol Zavidic <erolms@gmail.com>
2025-04-18 18:52:42 +0200

    build: fix linter configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,7 +8,32 @@ export default tseslint.config(
   tseslint.configs.strict,
   tseslint.configs.stylistic,
   {
+    // Lint the src and tests folder
+    files: [
+      'src/**/*.ts',
+      'src/**/*.js',
+      'tests/**/*.ts',
+      'tests/**/*.js',
+    ],
+  },
+  {
     // Exclude the dist folder from linting
     ignores: ['dist/**/*'],
+  },
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          "args": "all",
+          "argsIgnorePattern": "^_",
+          "caughtErrors": "all",
+          "caughtErrorsIgnorePattern": "^_",
+          "destructuredArrayIgnorePattern": "^_",
+          "varsIgnorePattern": "^_",
+          "ignoreRestSiblings": true,
+        }
+      ],
+    },
   },
 );
